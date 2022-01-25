0ea2d6fb58f79ad837bce95aa4ab932650f0af23
saisameerd <saisameer.d@gmail.com>
2022-01-25 12:46:41 +0530

    Organize function updated

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const fs = require('fs');
+const path = require('path');
+
 let inputArr = process.argv.slice(2);
 
 const command = inputArr[0];
@@ -22,7 +25,24 @@ function treeFn (dirPath){
 }
 
 function organizeFn (dirPath){
-    console.log("Organize has been implemented ", dirPath);
+   if(dirPath == undefined){
+       console.log("Please Enter Valid Path");
+       return;
+   }else{
+       let doesExist = fs.existsSync(dirPath);
+       if(doesExist){
+           let OF_path = path.join(dirPath,'Organized_Files');
+           if(fs.existsSync(OF_path)==false){
+            fs.mkdirSync(OF_path);
+            console.log('Organizer-Files folder created');
+           }else{
+            console.log('Organizer-Files folder already created');
+           }                 
+       }else{
+        console.log("Please Enter Valid Path");
+        return;
+       }
+   }
 }
 
 function helpFn(){
